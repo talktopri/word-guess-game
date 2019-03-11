c38f32de0b972f210f1ee581dc30fdb17ae2b989
Priscila Heller <priscila.x.p.r@gmail.com>
2019-03-11 16:44:17 -0400

    THE LOGIC WOOOOORKS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,105 @@
-// Creates an array of possible words
-    
-var hpWords = ["hogwarts", "hagrid", "gringotes", "voldemort", "weasley", "horcruxes", "testrals", "patronum", "leviosa", "aragorn"]
+var guessedLettersText = document.getElementById("guessed-letters-text");
+var guessesLeftText = document.getElementById("guesses-left");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var placeholder = document.getElementById("placeholder-text");
+var newGameButton = document.getElementById("new-game");
+var alertGameNotRunning = document.getElementById("game-not-running");
+var letterAlreadyPicked = document.getElementById("letter-already-picked");
 
-// the computer chooses a word randomly
-var computerWord = hpWords[Math.floor(Math.random() * hpWords.length)];
-console.log(computerWord);
+var hpWords = ["hogwarts", "hagrid", "gringotes", "voldemort", "weasley", "horcruxes", "testrals", "patronum", "leviosa", "aragorn", "harry potter"] //possible computer picks
 
-var GuessesText = document.getElementById("guesses-text");
-var livesText = document.getElementById("lives-text");
-livesText.textContent = "Remaining lives " + lives;
-var remainingGuesses = computerWord.length;
-var lives = remainingGuesses;
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var gameRunning = false;
+var computerWord = '';
+var computerWordPlaceHolder = [];
+var correctGuesses = [];
+var incorrectGuesses = [];
 
+function newGame () {
+    gameRunning = true;
+    guessesLeft = 10;
+    guessedLetters = [];
+    incorrectGuesses = [];
+    computerWordPlaceHolder = [];
 
-//Gets user's guess when the user types a letter
-document.onkeyup = function(event) {
-    var userLetter = event.key;
-    console.log(userLetter);
+    computerWord = hpWords[Math.floor(Math.random() * hpWords.length)];
+    console.log(computerWord);
+
+    for (var i = 0; i < computerWord.length; i++) {
+        if (computerWord[i] === " ") {
+            computerWordPlaceHolder.push(" ");
+        } else {
+            computerWordPlaceHolder.push(" _ ");
+        }
+    }
+    guessesLeftText.textContent = guessesLeft;
+    placeholder.textContent = computerWordPlaceHolder.join('');
+    guessedLettersText.textContent = incorrectGuesses;
 }
-// Creates an array to hold the users guesses
-var userGuesses =  [];
-for (i = 0; i < computerWord.length; i++) {
-    userGuesses[i] = "_";
-    console.log(userGuesses);
+
+function letterGuess(letter) {
+    console.log(letter);
+
+    if (gameRunning === true && guessedLetters.indexOf(letter) === -1) {
+        guessedLetters.push(letter);
+        
+        for (var i = 0; i < computerWord.length; i++) {
+            if (computerWord[i].toLowerCase() === letter.toLowerCase()) {
+                computerWordPlaceHolder[i] = computerWord[i];
+            }
+        }
+        
+        placeholder.textContent = computerWordPlaceHolder.join('');
+        checkIncorrect(letter);
     
-}
-
-while (remainingGuesses > 0) {
-    GuessesText.textContent = (userGuesses.join(" "));
-}    
-
-for (var w = 0; w < computerWord; w++) {
-    if (computerWord[w] === userLetter) {
-            userGuesses[w] = userLetter;
-            remainingGuesses--;
+    }
+    else {
+        if (!gameRunning) {
+            alertGameNotRunning.textContent = "You haven't started a game. Please press New Game to start guessing";
+        }
+        else {
+            letterAlreadyPicked.textContent = "You have already picked this letter. Try a new one!";
+        }
+        
     }
 }
-    
-    
-    
+
+function checkIncorrect (letter) {
+    if (computerWordPlaceHolder.indexOf(letter.toLowerCase()) === -1) {
+    guessesLeft--;
+    incorrectGuesses.push(letter);
+    guessedLettersText.textContent = incorrectGuesses.join(' ');
+    guessesLeftText.textContent = guessesLeft;
+    }
+checkLoss();
+}
+
+function checkLoss() {
+    if (guessesLeft === 0) {
+        losses++;
+        gameRunning = false;
+        lossesText.textContent = losses;
+        placeholder.textContent = computerWord;
+    }
+checkWin();    
+}
+
+function checkWin() {
+    if (computerWord.toLowerCase() === computerWordPlaceHolder.join('').toLowerCase()) {
+        wins++;
+        gameRunning = false;
+        winsText.textContent = wins;
+    }
+}
 
 
+newGameButton.addEventListener("click", newGame);
 
-// // set variables that hold references to the places in the HTML where we want to display things.
-// var winsText = document.getElementById("wins-text");
-// var gameOverText = document.getElementById("gameover-text");
-// var userLettersText = document.getElementById("user-letters-text");
-// 
-// 
-
-// // Display the user guesses, and wins/losses/remaining guesses.
-// userLettersText.textContent = "You chose: " + wrongLetter;
-// 
-// winsText.textContent = "Wins: " + wins;
-
-// // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-// var wins = 0;
-// var remainingGuesses = 10;
-
-
-
-// var wrongLetter = [];
-
-// // Tests if it works so far
-// console.log(computerWord);
-
-
-
-
-// console.log(wordDashes());
-
-
-
-
-// letterHolder.forEach (function (element)) {
-//     if (userLetter.contains(element)) {
-//         rightLetter.push(userLetter);
-//     }
-// }
-// if (computerWord.indexOf(userLetter) > -1) {   
-//         rightLetter.push(userLetter);
-//         console.log(rightLetter);
-//     // replace dashes with rightLetter    
-//     letterHolder[computerWord.indexOf(userLetter)] = userLetter;
-//     }
-
-//     else if (computerWord.indexOf(userLetter) === -1) {
-//         wrongLetter.push(userLetter) && remainingGuesses--;
-//         console.log(wrongLetter);
-//     }
-        
-
-//     if (letterHolder.join('') === computerWord) {
-//         alert("You Win!");
-//     }
-
-//     else if (remainingGuesses === 0) {
-//         gameOverText.textContent = "Game Over!"
-//     }
-// }
-
-
-// randomWord.textContent = 
-
-//TO_DO
-// - Make sure remainingGuesses shows correctly. Currently it shows as 10 even if I guess an incorrect letter
-// - Make sure a typed letter only counts once. Currently it counts as many times as you type them
-// If a word has repeated letters (like gringotes), if a user types 'g' once, it should be read once and pushed as many times as it appears to the correct place
-// contains, matches, maybe a function: if there is more than one occurrence of a letter, push it that
-// if wrongLetter + rightLetter = 10, remainingGuesses should be 0 and gameover
+document.onkeyup = function(event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        letterGuess(event.key);
+    }
+}
